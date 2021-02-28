f16cf45e79c0bc2b34771a1b7fc00197697f4bd9
Brunno Costa <brunno.costa@sotreq.com.br>
2021-02-28 13:47:35 -0300

    include cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import courses from './routes/courses.js'
+import cors from 'cors'
 import { promises as fs } from "fs";
 
 const { readFile, writeFile } = fs;
 
 const app = express();
+
+app.use(cors())
 app.use(express.json())
 app.use('/laboratorio', courses)
 
