0720639affc407c62198984b4de9741cbfe41571
Brunno Costa <brunno.costa@sotreq.com.br>
2021-02-28 13:27:47 -0300

    rotas CRUD

--- index.js
+++ index.js
@@ -1,90 +1,22 @@
 import express from 'express'
-import fs from 'fs'
-import cors from 'cors'
+import courses from './routes/courses.js'
+import { promises as fs } from "fs";
 
-//app.set('port', (process.env.PORT || 5000));
+const { readFile, writeFile } = fs;
 
-const app = express()
-app.use(cors())
-//const port = 3001
+const app = express();
+app.use(express.json())
+app.use('/laboratorio', courses)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/allCourses', async (req, res) => {
+app.listen(process.env.PORT || 3001, async () => {
   try {
-    const data = JSON.parse(await fs.readFileSync('moldeCursos.json'))
-    res.send(data)
-    res.end();
+    await readFile("moldeCursos.json")
+    console.log()
+    console.log("########################")
+    console.log("##### API started ######")
+    console.log("########################")
+    console.log()
   } catch (err) {
-    res.status(400).send({ error: err.message })
+    console.log(err);
   }
-})
-
-
-app.get('/categories', async (req, res) => {
-  const categorias = []
-  const categoriasMaked = []
-  try {
-    const data = JSON.parse(await fs.readFileSync('moldeCursos.json'))
-
-    data.cursos.forEach(element => {
-      if (categorias.indexOf(element.categoria) === -1) {
-        categorias.push(element.categoria)
-      }
-    });
-
-    categorias.forEach(element => {
-      const achei = data.cursos.find(el => el.categoria == element)
-      categoriasMaked.push(achei)
-    });
-
-    res.send(categoriasMaked)
-    res.end();
-  } catch (err) {
-    res.status(400).send({ error: err.message })
-  }
-})
-
-
-app.get('/course/:id', async (req, res) => {
-  try {
-    const cursos = JSON.parse(await fs.readFileSync("moldeCursos.json"))
-    const resultCursos = cursos.cursos.find(curso => curso.id === req.params.id)
-    res.send(resultCursos)
-
-    res.end();
-  } catch (err) {
-    res.status(400).send({ error: err.message })
-  }
-})
-
-
-
-app.get('/trails', async (req, res) => {
-  const categorias = []
-  const categoriasMaked = []
-  try {
-    const data = JSON.parse(await fs.readFileSync('moldeCursos.json'))
-
-    data.cursos.forEach(element => {
-      if (categorias.indexOf(element.categoria) === -1) {
-        categorias.push(element.categoria)
-      }
-    });
-
-    res.send(categorias.sort())
-    res.end();
-  } catch (err) {
-    res.status(400).send({ error: err.message })
-  }
-})
-
-
-
-
-
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`API Iniciada`)
 })
---
+++ routes/courses.js
@@ -0,0 +1,178 @@
+import express from 'express'
+import { promises as fs } from "fs";
+import crypto from "crypto";
+
+const { readFile, writeFile } = fs;
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.get('/allCourses', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile('moldeCursos.json'))
+    res.send(data)
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+})
+
+
+router.get('/categories', async (req, res) => {
+  const categorias = []
+  const categoriasMaked = []
+  try {
+    const data = JSON.parse(await readFile('moldeCursos.json'))
+
+    data.cursos.forEach(element => {
+      if (categorias.indexOf(element.categoria) === -1) {
+        categorias.push(element.categoria)
+      }
+    });
+
+    categorias.forEach(element => {
+      const achei = data.cursos.find(el => el.categoria == element)
+      categoriasMaked.push(achei)
+    });
+
+    res.send(categoriasMaked)
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+})
+
+
+router.get('/course/:id', async (req, res) => {
+  try {
+    const cursos = JSON.parse(await readFile("moldeCursos.json"))
+    const resultCursos = cursos.cursos.find(curso => curso.id === req.params.id)
+    res.send(resultCursos)
+
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+})
+
+
+
+router.get('/trails', async (req, res) => {
+  const categorias = []
+  const categoriasMaked = []
+  try {
+    const data = JSON.parse(await readFile('moldeCursos.json'))
+
+    data.cursos.forEach(element => {
+      if (categorias.indexOf(element.categoria) === -1) {
+        categorias.push(element.categoria)
+      }
+    });
+
+    res.send(categorias.sort())
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+})
+
+router.delete("/deletar/:id", async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile('moldeCursos.json'))
+    const verificaSeAchou = data.cursos.find(curso => curso.id === req.params.id)
+    if (verificaSeAchou == null) {
+      const result = `Não Encontrou o Curso:  ${req.params.id}`
+      res.send(result)
+    } else {
+      data.cursos = data.cursos.filter(curso => curso.id !== req.params.id)
+      writeFile('moldeCursos.json', JSON.stringify(data))
+      res.send(data)
+    }
+
+    res.end()
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+})
+
+router.put('/altera', async (req, res) => {
+  try {
+    const dataBody = req.body;
+    const data = JSON.parse(await fs.readFile('moldeCursos.json'))
+    const index = data.cursos.findIndex(curso => curso.id === dataBody.id);
+
+
+
+    data.cursos[index] = {
+      "id": dataBody.id,
+      "nomeCurso": dataBody.nomeCurso,
+      "conteudoCurso": data.cursos[index].conteudoCurso,
+      "idioma": data.cursos[index].idioma,
+      "emailProdutor": data.cursos[index].emailProdutor,
+      "formaDeAcesso": dataBody.formaDeAcesso,
+      "formato": dataBody.formato,
+      "categoria": dataBody.categoria,
+      "precoOriginal": data.cursos[index].precoOriginal,
+      "preco": dataBody.preco,
+      "temperatura": data.cursos[index].temperatura,
+      "blueprint": data.cursos[index].blueprint,
+      "satisfacao": data.cursos[index].satisfacao,
+      "thumb": data.cursos[index].thumb,
+      "divulgacao": {
+        "site": data.cursos[index].divulgacao.site
+      },
+      "comprar": {
+        "hotlink": data.cursos[index].comprar.hotlink
+      }
+    }
+
+    await fs.writeFile('grades.json', JSON.stringify(data));
+
+    res.send(data);
+    res.end();
+
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+
+})
+
+
+router.post('/insere', async (req, res) => {
+  try {
+    let jsonBody = req.body;
+
+    const json = JSON.parse(await fs.readFile('moldeCursos.json'))
+    const verificaSeAchou = json.cursos.find(curso => curso.nomeCurso.toLowerCase() === jsonBody.nomeCurso.toLowerCase());
+
+
+    let guid = crypto.randomBytes(16).toString("hex");
+
+    if (verificaSeAchou == null) {
+      jsonBody = { id: guid, ...jsonBody }
+      json.cursos.push(jsonBody)
+      await fs.writeFile('moldeCursos.json', JSON.stringify(json))
+
+      res.send(jsonBody)
+    } else {
+      const result = `O Curso ${jsonBody.nomeCurso} já está Cadastrado`
+      res.send(result)
+    }
+
+    res.end();
+
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
+
+})
+
+
+
+
+
+
+export default router;
